d548d2261d67acaf2d91148e74b5c4df0bccb53d
Jelson Matheus <jelsonmatheus200f@gmail.com>
2022-08-06 22:22:59 -0300

    Adiciona endpoint de relatorios

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, register_converter
 from rest_framework import routers
 
 from api import views
@@ -6,9 +6,17 @@ from api import views
 
 router = routers.DefaultRouter()
 router.register('survivors', views.SurvivorList, 'suvivors')
-router.register('infected-reports', views.ReportInfectedView, 'infected-reports')
+router.register('report-contamination', views.ReportInfectedView, 'infected-reports')
+
+urlreports = [
+    path('infected/', views.InfectedReportsView.as_view(), name='report-infected'),
+    path('uninfected/', views.UninfectedReportsView.as_view(), name='report-uninfected'),
+    path('avg-resources/', views.AverageResourcesView.as_view(), name='avg-resources'),
+    path('lost-points/', views.LostPointsView.as_view(), name='lost-points')
+]
 
 urlpatterns = [
     path('', include(router.urls)),
     path('trades/', views.TradeView.as_view(), name='trade'),
+    path('reports/', include(urlreports)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django import views
 from django.utils.translation import gettext_lazy as _
+from django.db.models import Count, Sum, Q
 from rest_framework import viewsets, mixins, views
 from rest_framework.response import Response
 
@@ -33,4 +34,68 @@ class TradeView(views.APIView):
         if serializer.is_valid(True):
             serializer.save()
             return Response(serializer.data)
-            
+
+
+class InfectedReportsView(views.APIView):
+    queryset = Survivor.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        infected = Count('pk', filter=Q(is_infected=True))
+        query = self.queryset.aggregate(total=Count('pk'), infected=infected)
+        value = query['infected'] / query['total']
+        data = {
+            'infected': query['infected'],
+            'percentage': "{:.2%}".format(value)
+        }
+        return Response(data)
+
+
+class UninfectedReportsView(views.APIView):
+    queryset = Survivor.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        uninfected = Count('pk', filter=Q(is_infected=False))
+        query = self.queryset.aggregate(total=Count('pk'), uninfected=uninfected)
+        value = query['uninfected'] / query['total']
+        data = {
+            'uninfected': query['uninfected'],
+            'percentage': "{:.2%}".format(value)
+        }
+        return Response(data)
+
+class AverageResourcesView(views.APIView):
+    queryset = Inventory.objects.all()
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = self.queryset.filter(survivor__is_infected=False)
+        items = Inventory.ItemValue.labels
+        params = {item.lower(): Sum(item.lower()) for item in items}
+
+        return queryset.aggregate(total_survivor=Count('pk'), **params)
+
+    def get(self, request, *args, **kwargs):
+        query = self.get_queryset(*args, **kwargs)
+        total = query.pop('total_survivor')
+        data = {}
+        for key, value in query.items():
+            label = f"avg_{key}"
+            data[label] = value / total
+        return Response(data)
+
+
+class LostPointsView(views.APIView):
+    queryset = Inventory.objects.all()
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = self.queryset.filter(survivor__is_infected=True)
+        items = Inventory.ItemValue.labels
+        params = {item.lower(): Sum(item.lower()) for item in items}
+
+        return queryset.aggregate(**params)
+
+    def get(self, request, *args, **kwargs):
+        query = self.get_queryset(*args, **kwargs)
+        data = {
+            'lost_points': sum(query.values())
+        }
+        return Response(data)
