e561dde2dcb7841abf6760409b077360c6eb8f60
Jelson Matheus <jelsonmatheus200f@gmail.com>
2022-08-08 17:15:56 -0300

    Fix settings

--- zssn/settings.py
+++ zssn/settings.py
@@ -83,14 +83,6 @@ WSGI_APPLICATION = 'zssn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Database
 if ENVIRONMENT == 'PROD':
     DATABASE_URL = config('DATABASE_URL')
 
